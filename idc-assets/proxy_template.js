8f1a29b930b0c30a2d07a09b0611af5a5d725b80
wlongabaugh <wlongabaugh@systemsbiology.org>
2021-08-13 17:41:43 -0700

    change config

--- idc-assets/proxy_template.js
+++ idc-assets/proxy_template.js
@@ -1,11 +1,10 @@
 
 window.config = {
-  path: "/",
-  routerBasename: "/slim",
+  path: "/slim",
   /** This is an array, but we'll only use the first entry for now */
   servers: [
     {
-      id: "proxy",
+      id: "idc",
       url: '_X___IDC__Z__DICOM___URL___Y_',
       write: false
     }
